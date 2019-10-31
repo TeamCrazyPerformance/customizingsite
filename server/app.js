3493f75d9a783c619d41231ff5a0d20b3a592cb6
KUvH <kuvh@live.co.kr>
2019-11-01 04:52:22 +0900

    [Update Feature] DB Connection

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
+var db = require('./db');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
