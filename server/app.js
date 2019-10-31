280483802776eced98cb258a29a6a189f85a9f72
KUvH <kuvh@live.co.kr>
2019-11-01 04:34:38 +0900

    [Update Feature] Middleware Setting for Using Body Parser

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -9,10 +9,14 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'content-type, x-access-token');
+    next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
