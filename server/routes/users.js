268eda64fe68e74710dac5fc6c5a4cf9852dc3c8
KUvH <kuvh@live.co.kr>
2019-11-01 06:01:03 +0900

    [Create Feature] Register User

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var bcrypt = require('bcrypt-nodejs');
+var db = require('../db');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.post('/register', function(req, res, next) {
+  var user = {
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password),
+    nickname: req.body.nickname
+  };
+
+  var query = "SELECT email FROM ?? WHERE ??=?";
+  var table = ["user", "email", user.email];
+  query = mysql.format(query, table);
+
+  db.query(query, function(err, rows) {
+    if(err) {
+      res.status(500);
+      res.json({"errorMsg" : "Database connection error"});
+    } else {
+      if(rows.length === 0) {
+        var query = "INSERT INTO ?? SET ?";
+        var table = ["user"];
+        query = mysql.format(query, table);
+
+        db.query(query, user, function(err, rows) {
+          if (err) {
+            res.status(500);
+            res.json({"errorMsg" : "Cannot register user"});
+          } else {
+            res.status(201);
+            res.json({"message" : "Success"});
+          }
+        });
+      } else {
+        res.status(406);
+        res.json({"errorMsg" : "Email already exists"});
+      }
+    }
+  })
+
 });
 
 module.exports = router;
