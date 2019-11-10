473d75a1e6046baefb0903cea0680786af63342e
hyeminJu <49295445+hyeminJu@users.noreply.github.com>
2019-11-11 02:59:28 +0900

    [Add] Initial Bookmark Function
    clock and popUp

---
+++ client/js/bookmark-clock.js
@@ -0,0 +1,26 @@
+const clockContainer= document.querySelector(".bookmark-clock"),
+    clockTime =clockContainer.querySelector(".bookmark-time"),
+    clockDate =clockContainer.querySelector(".bookmark-date");
+
+
+function clock(){
+    const clock= new Date();
+
+    const year = clock.getFullYear();
+    const month = clock.getMonth();
+    const day = clock.getDate();
+    const hours = clock.getHours();
+    const minutes = clock.getMinutes();
+    const seconds = clock.getSeconds();
+
+    clockDate.innerText = `${year}.${month<10? `0${month}`: month}.${day<10? `0${day}`:day}`;
+    clockTime.innerText = `${hours <10? `0${hours}`: hours} : ${minutes<10? `0${minutes}`: minutes } : ${seconds<10? `0${seconds}` : seconds}`;
+}
+
+function init() {
+    clock();
+    setInterval(clock, 1000);
+
+}
+
+init();
