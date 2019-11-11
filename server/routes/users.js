1ebfa089d2a5d05e5d03434828e34defba23f35d
KUvH <kuvh@live.co.kr>
2019-11-11 15:38:03 +0900

    [Add Feature] Register Request Validator

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,41 +3,56 @@ var router = express.Router();
 var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
 var db = require('../db');
+const { check, validationResult } = require('express-validator');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/register', function(req, res, next) {
-  var user = {
-    email: req.body.email,
-    password: bcrypt.hashSync(req.body.password),
-    nickname: req.body.nickname
-  };
+router.post('/register', [
+    check('email').isEmail(),
+    check('password').isString(),
+    check('password').isLength({ min: 4, max: 20 }),
+    check('nickname').isString(),
+    check('password').isLength({ min: 2, max: 16 }),
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errorMsg: "Bad Parameter", errors: errors.array() })
+  }
 
-  var query = "SELECT email FROM ?? WHERE ??=?";
-  var table = ["user", "email", user.email];
+  const email = req.body.email;
+  const password = bcrypt.hashSync(req.body.password);
+  const nickname = req.body.nickname;
+
+  let query = "SELECT email FROM ?? WHERE ??=?";
+  const table = ["user", "email", email];
   query = mysql.format(query, table);
 
   db.query(query, function(err, rows) {
     if(err) {
-      res.status(500).json({"errorMsg" : "Database connection error"});
+      res.status(500).json({errorMsg : "Database connection error"});
     } else {
       if(rows.length === 0) {
-        var query = "INSERT INTO ?? SET ?";
-        var table = ["user"];
+        let query = "INSERT INTO ?? SET ?";
+        const table = ["user"];
         query = mysql.format(query, table);
 
+        const user = {
+          email: email,
+          password: password,
+          nickname: nickname
+        };
         db.query(query, user, function(err, rows) {
           if (err) {
-            res.status(500).json({"errorMsg" : "Cannot register user"});
+            res.status(500).json({errorMsg : "Cannot register user"});
           } else {
-            res.status(201).json({"message" : "Success"});
+            res.status(201).json({message : "Success"});
           }
         });
       } else {
-        res.status(406).json({"errorMsg" : "Email already exists"});
+        res.status(406).json({errorMsg : "Email already exists"});
       }
     }
   })
