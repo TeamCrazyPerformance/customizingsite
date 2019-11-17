385df28127ae11fe531e6228d83f42de826712fc
KUvH <kuvh@live.co.kr>
2019-11-17 18:27:30 +0900

    [Add] Auth Middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,17 @@
+const config = require('../config');
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+    try {
+        req.decoded = jwt.verify(req.headers.authorization, config.secret);
+        return next();
+    } catch (e) {
+        if (e.name === 'TokenExpiredError') {
+            return res.status(419).json({errorMsg : "Token Expired"});
+        }
+
+        return res.status(401).json({errorMsg : "Invalid Token"})
+    }
+};
+
+module.exports = authMiddleware;
