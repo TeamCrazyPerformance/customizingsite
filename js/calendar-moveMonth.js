02609d79d8f6c0ab2324dd80e8e0ba0c9bdfa85d
dm Go <hm27142712@gmail.com>
2019-11-30 19:24:26 +0900

    [Create Page] Portfolio

--- js/calendar-moveMonth.js
+++ js/calendar-moveMonth.js
@@ -1,65 +1,79 @@
-function prev(){
+function movePrev() {
+    // 투두리스트 삭제
     inputBox.value = "";
     const $divs = document.querySelectorAll('#input-list > div');
-    $divs.forEach(function(e){
+    $divs.forEach(function (e) {
         e.remove();
     });
     const $btns = document.querySelectorAll('#input-list > button');
-    $btns.forEach(function(e1){
+    $btns.forEach(function (e1) {
         e1.remove();
     });
-    if(pageFirst.getMonth() === 1){
-        pageFirst = new Date(first.getFullYear()-1, 12, 1);
+
+    //1월이면 연도랑 달 모두 바꾸
+    if (pageFirst.getMonth() === 1) {
+        pageFirst = new Date(first.getFullYear() - 1, 12, 1);
         first = pageFirst;
-        if(first.getFullYear() % 4 === 0){
+        if (first.getFullYear() % 4 === 0) {
             pageYear = leapYear;
-        }else{
+        } else {
             pageYear = notLeapYear;
         }
-    }else{
-        pageFirst = new Date(first.getFullYear(), first.getMonth()-1, 1);
+    } else {
+        pageFirst = new Date(first.getFullYear(), first.getMonth() - 1, 1);
         first = pageFirst;
     }
-    today = new Date(today.getFullYear(), today.getMonth()-1, today.getDate());
-    currentTitle.innerHTML = monthList[first.getMonth()] + '&nbsp;&nbsp;&nbsp;&nbsp;'+ first.getFullYear();
+    today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
+
+
+    currentTitle.innerHTML = monthList[first.getMonth()] + '&nbsp;&nbsp;&nbsp;&nbsp;' + first.getFullYear();
     removeCalendar();
     showCalendar();
-    showMain();
-    clickedDate1 = document.getElementById(today.getDate());
+    showMain(today);
+    clickedDate1 = document.getElementById('today.getDate()');
     clickedDate1.classList.add('active');
     clickStart();
     reshowingList();
 }
 
-function next(){
+function moveNext() {
+    // 투두리스트 삭제
     inputBox.value = "";
     const $divs = document.querySelectorAll('#input-list > div');
-    $divs.forEach(function(e){
+    $divs.forEach(function (e) {
         e.remove();
     });
     const $btns = document.querySelectorAll('#input-list > button');
-    $btns.forEach(function(e1){
+    $btns.forEach(function (e1) {
         e1.remove();
     });
-    if(pageFirst.getMonth() === 12){
-        pageFirst = new Date(first.getFullYear()+1, 1, 1);
+
+    // 12월이면 연도랑 달 모두 바꾸기
+    if (pageFirst.getMonth() === 12) {
+        pageFirst = new Date(first.getFullYear() + 1, 1, 1);
         first = pageFirst;
-        if(first.getFullYear() % 4 === 0){
+        if (first.getFullYear() % 4 === 0) {
             pageYear = leapYear;
-        }else{
+        } else {
             pageYear = notLeapYear;
         }
-    }else{
-        pageFirst = new Date(first.getFullYear(), first.getMonth()+1, 1);
+    } else {
+        pageFirst = new Date(first.getFullYear(), first.getMonth() + 1, 1);
         first = pageFirst;
     }
     today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
-    currentTitle.innerHTML = monthList[first.getMonth()] + '&nbsp;&nbsp;&nbsp;&nbsp;'+ first.getFullYear();
+
+    currentTitle.innerHTML = monthList[first.getMonth()] + '&nbsp;&nbsp;&nbsp;&nbsp;' + first.getFullYear();
     removeCalendar();
     showCalendar();
-    showMain();
-    clickedDate1 = document.getElementById(today.getDate());
+    showMain(today);
+    clickedDate1 = document.getElementById('today.getDate()');
     clickedDate1.classList.add('active');
     clickStart();
     reshowingList();
+}
+
+function changeMonth() {
+    prev.addEventListener('click', movePrev);
+    next.addEventListener('click', moveNext);
 }
---
+++ js/portfolio.js
@@ -0,0 +1,9 @@
+/*
+-------- js --------
+    3. popup
+4. file upload
+5. photo upload
+6.
+
+
+ */
