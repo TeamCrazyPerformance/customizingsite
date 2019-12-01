2d39937ef1cac294f4d98b9f4601413a2d948e0e
Hyemin <hm2712@icloud.com>
2019-12-01 23:58:20 +0900

    [Create Page] Portfolio

--- client/js/calendar-moveMonth.js
+++ client/js/calendar-moveMonth.js
@@ -1,39 +1,79 @@
-function prev() {
-    // 투두리스트 리셋
+function movePrev() {
+    // 투두리스트 삭제
     inputBox.value = "";
-    const $divs = document.querySelectorAll('#input-list >div');
+    const $divs = document.querySelectorAll('#input-list > div');
     $divs.forEach(function (e) {
         e.remove();
-
     });
-    const $btns = document.querySelectorAll('#input-list>button');
+    const $btns = document.querySelectorAll('#input-list > button');
     $btns.forEach(function (e1) {
         e1.remove();
     });
-    // 1월인 경우 예외처리
-    if(pageFirst.getMonth()===1){
-        pageFirst = new Date(first.getFullYear()-1, 12, 1);
+
+    //1월이면 연도랑 달 모두 바꾸
+    if (pageFirst.getMonth() === 1) {
+        pageFirst = new Date(first.getFullYear() - 1, 12, 1);
         first = pageFirst;
-        // 윤년 처리까지 ..
-        if(first.getFullYear() %4 === 0){
+        if (first.getFullYear() % 4 === 0) {
             pageYear = leapYear;
-        }else{
+        } else {
             pageYear = notLeapYear;
         }
     } else {
-        pageFirst = new Date(first.getFullYear(), first.getMonth()-1, 1);
+        pageFirst = new Date(first.getFullYear(), first.getMonth() - 1, 1);
         first = pageFirst;
     }
+    today = new Date(today.getFullYear(), today.getMonth() - 1, today.getDate());
 
-    today = new Date(today.getFullYear(), today.getMonth()-1, today.getDate());
-    currentTitle.innerHTML = monthList[first.getMonth()]+'&nbsp;&nbsp;&nbsp;'+ first.getFullYear();
+
+    currentTitle.innerHTML = monthList[first.getMonth()] + '&nbsp;&nbsp;&nbsp;&nbsp;' + first.getFullYear();
     removeCalendar();
     showCalendar();
-
-    // 나중에 만들 변수랑 함수들 
-    showMain();
-     clickedDate1 = document.getElementById(today.getDate());
-     clicedDate1.classList.add('active');
+    showMain(today);
+    clickedDate1 = document.getElementById('today.getDate()');
+    clickedDate1.classList.add('active');
     clickStart();
     reshowingList();
+}
+
+function moveNext() {
+    // 투두리스트 삭제
+    inputBox.value = "";
+    const $divs = document.querySelectorAll('#input-list > div');
+    $divs.forEach(function (e) {
+        e.remove();
+    });
+    const $btns = document.querySelectorAll('#input-list > button');
+    $btns.forEach(function (e1) {
+        e1.remove();
+    });
+
+    // 12월이면 연도랑 달 모두 바꾸기
+    if (pageFirst.getMonth() === 12) {
+        pageFirst = new Date(first.getFullYear() + 1, 1, 1);
+        first = pageFirst;
+        if (first.getFullYear() % 4 === 0) {
+            pageYear = leapYear;
+        } else {
+            pageYear = notLeapYear;
+        }
+    } else {
+        pageFirst = new Date(first.getFullYear(), first.getMonth() + 1, 1);
+        first = pageFirst;
+    }
+    today = new Date(today.getFullYear(), today.getMonth() + 1, today.getDate());
+
+    currentTitle.innerHTML = monthList[first.getMonth()] + '&nbsp;&nbsp;&nbsp;&nbsp;' + first.getFullYear();
+    removeCalendar();
+    showCalendar();
+    showMain(today);
+    clickedDate1 = document.getElementById('today.getDate()');
+    clickedDate1.classList.add('active');
+    clickStart();
+    reshowingList();
+}
+
+function changeMonth() {
+    prev.addEventListener('click', movePrev);
+    next.addEventListener('click', moveNext);
 }
--- client/js/calendar-show.js
+++ client/js/calendar-show.js
@@ -1,42 +1,39 @@
-const currentTitle = document.getElementById('current-year-month');
-const calendarBody = document.getElementById('calendar-body');
-// 오늘 날짜, 연도, 요일 정보 다 담기
-let today = new Date();
-// 첫날 설정, 이제 getYear 아니고 getFullYear 임
-let first = new Date(today.getFullYear(), today.getMonth(),1);
+var currentTitle = document.getElementById('current-year-month');
+var calendarBody = document.getElementById('calendar-body');
+var today = new Date();
+var first = new Date(today.getFullYear(), today.getMonth(), 1);
+var dayList = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+var leapYear = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var notLeapYear = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var pageFirst = first;
+var pageYear;
 
-// 요일, 달, 윤년 데이 저장
-const dayList= ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-const monthList= ['January','February','March','April','May','June','July','August','September','October','November','December'];
-const leapYear = [31,29,31,30,31,30,31,31,30,31,30,31];
-const notLeapYear = [31,28,31,30,31,30,31,31,30,31,30,31];
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+var inputBox = document.getElementById('input-box');
 
-// 출력할 폼이랑 정보 => 년도랑 요일들
-let pageFirst = first;
-let pageYear;
-    //윤년일 경우
-    if(first.getFullYear()%4 ===0){
-        pageYear=leapYear;
-    }else{
-        pageYear=notLeapYear;
-    }
+var mainTodayDay = document.getElementById('main-day');
+var mainTodayDate = document.getElementById('main-date');
+
+if (first.getFullYear() % 4 === 0) {
+    pageYear = leapYear;
+} else {
+    pageYear = notLeapYear;
+}
 
 function showCalendar() {
     let monthCnt = 100;
-   let cnt = 1;
-    // 최대 6주 출력 (row)
-    for (let i=0;i<6;i++){
-        let $tr = document.createElement('tr');
+    let cnt = 1;
+    for (var i = 0; i < 6; i++) {
+        var $tr = document.createElement('tr');
         $tr.setAttribute('id', monthCnt);
-        // 7일 출력 (column)
-        for(let j=0; j<7; j++){
-            // 첫주에 시작하는 요일부터 날짜 출력 + 달력 출력 종료 조건 = 빈칸으로 행 채우기
-            if((i=== 0 &&first.getDay()) || cnt>pageYear[first.getMonth()]){
-                let $td = document.createElement('td');
+        for (var j = 0; j < 7; j++) {
+            if ((i === 0 && j < first.getDay()) || cnt > pageYear[first.getMonth()]) {
+                var $td = document.createElement('td');
                 $tr.appendChild($td);
-            }
-            else{
-                let $td = document.createElement('td');
+            } else {
+                var $td = document.createElement('td');
                 $td.textContent = cnt;
                 $td.setAttribute('id', cnt);
                 $tr.appendChild($td);
@@ -47,14 +44,31 @@ function showCalendar() {
         calendarBody.appendChild($tr);
     }
 
+    currentTitle.innerHTML = monthList[first.getMonth()] + '&nbsp;&nbsp;&nbsp;&nbsp;' + first.getFullYear();
 }
-    showCalendar();
 
-    function removeCalendar() {
-        let catchTr = 100;
-        for(let i=100;i<106;i++){
-            let $tr = document.getElementById(catchTr);
-            $tr.remove();
-            catchTr++;
-        }
+
+
+function removeCalendar() {
+    let catchTr = 100;
+    for (var i = 100; i < 106; i++) {
+        var $tr = document.getElementById(catchTr);
+        $tr.remove();
+        catchTr++;
     }
+}
+
+$(document).ready(function () {
+    showCalendar();
+    showMain(today);
+    clickedDate1 = document.getElementById(today.getDate());
+    clickedDate1.classList.add('active');
+    clickStart();
+    reshowingList();
+});
+
+
+// node =>  export require()
+// 전역 변수 다 함수 안에 넣기
+// 함수 안에 있지 않은건 다 애기
+
