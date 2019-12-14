c2200192afd3f0aa85a6b8f8f8713579eee210e3
KUvH <kuvh@live.co.kr>
2019-12-14 18:02:12 +0900

    [Create Feature] Save Bookmark Info

--- server/routes/bookmark.js
+++ server/routes/bookmark.js
@@ -75,4 +75,76 @@ router.get('/', authMiddleware, function(req, res, next) {
     });
 });
 
+// Edit or Create Bookmark
+router.post('/', authMiddleware, [
+    check('title').isString(),
+    check('description').isString(),
+    check('isPublic').isBoolean()
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errorMsg: "Bad Parameter", errors: errors.array() })
+    }
+
+    const email = req.decoded.email;
+
+    let query = "SELECT user_id, email FROM ?? WHERE ??=?";
+    const table = ["user", "email", email];
+    query = mysql.format(query, table);
+
+    db.query(query, function(err, rows) {
+        if(err) {
+            res.status(500).json({errorMsg : "Database connection error"});
+        } else {
+            if(rows.length === 1) {
+                const user_id = rows[0].user_id;
+                let query = "SELECT user_id FROM ?? WHERE ??=?";
+                const table = ["bookmark", "user_id", user_id];
+                query = mysql.format(query, table);
+
+                db.query(query, function(err, rows) {
+                    if (err) {
+                        res.status(500).json({errorMsg: "Database connection error"});
+                    } else {
+                        if(rows.length === 1) {
+                            let query = "UPDATE bookmark SET title = ?, description = ?, public = ? WHERE user_id=?";
+                            const table = [req.body.title, req.body.description, req.body.isPublic, user_id];
+                            query = mysql.format(query, table);
+
+                            db.query(query, function(err, rows) {
+                                if (err) {
+                                    res.status(500).json({errorMsg : "Cannot Save Bookmark"});
+                                } else {
+                                    res.json({message : "Success"});
+                                }
+                            });
+                        } else {
+                            let query = "INSERT INTO ?? SET ?";
+                            const table = ["bookmark"];
+                            query = mysql.format(query, table);
+
+                            const bookmark = {
+                                user_id: user_id,
+                                title: req.body.title,
+                                description: req.body.description,
+                                public: req.body.isPublic
+                            };
+
+                            db.query(query, bookmark, function(err, rows) {
+                                if (err) {
+                                    res.status(500).json({errorMsg : "Cannot Save Bookmark"});
+                                } else {
+                                    res.status(201).json({message : "Success"});
+                                }
+                            });
+                        }
+                    }
+                });
+            } else {
+                res.status(404).json({errorMsg : "User Not Found"});
+            }
+        }
+    });
+});
+
 module.exports = router;
