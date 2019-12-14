7e99d57cb4444de19ef50f86fef42789f8773e9c
KUvH <kuvh@live.co.kr>
2019-12-14 22:01:47 +0900

    [Create Feature] Modify Calendar Info

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -46,4 +46,72 @@ router.get('/', authMiddleware, function(req, res, next) {
     });
 });
 
+// Edit or Create Calendar
+router.post('/', authMiddleware, [
+    check('isPublic').isBoolean()
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errorMsg: "Bad Parameter", errors: errors.array() })
+    }
+
+    const email = req.decoded.email;
+
+    let query = "SELECT user_id, email FROM ?? WHERE ??=?";
+    const table = ["user", "email", email];
+    query = mysql.format(query, table);
+
+    db.query(query, function(err, rows) {
+        if(err) {
+            res.status(500).json({errorMsg : "Database connection error"});
+        } else {
+            if(rows.length === 1) {
+                const user_id = rows[0].user_id;
+                let query = "SELECT user_id FROM ?? WHERE ??=?";
+                const table = ["calendar", "user_id", user_id];
+                query = mysql.format(query, table);
+
+                db.query(query, function(err, rows) {
+                    if (err) {
+                        res.status(500).json({errorMsg: "Database connection error"});
+                    } else {
+                        if(rows.length === 1) {
+                            let query = "UPDATE calendar SET public = ? WHERE user_id=?";
+                            const table = [req.body.isPublic, user_id];
+                            query = mysql.format(query, table);
+
+                            db.query(query, function(err, rows) {
+                                if (err) {
+                                    res.status(500).json({errorMsg : "Cannot Save Calendar"});
+                                } else {
+                                    res.json({message : "Success"});
+                                }
+                            });
+                        } else {
+                            let query = "INSERT INTO ?? SET ?";
+                            const table = ["calendar"];
+                            query = mysql.format(query, table);
+
+                            const calendar = {
+                                user_id: user_id,
+                                public: req.body.isPublic
+                            };
+
+                            db.query(query, calendar, function(err, rows) {
+                                if (err) {
+                                    res.status(500).json({errorMsg : "Cannot Save Calendar"});
+                                } else {
+                                    res.status(201).json({message : "Success"});
+                                }
+                            });
+                        }
+                    }
+                });
+            } else {
+                res.status(404).json({errorMsg : "User Not Found"});
+            }
+        }
+    });
+});
+
 module.exports = router;
