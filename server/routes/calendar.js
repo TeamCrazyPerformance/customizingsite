307012080c83c3e7e934b8961ad3aa1cb7209121
KUvH <kuvh@live.co.kr>
2019-12-14 22:22:02 +0900

    [Create Feature] Get Calendar Item By Date

--- server/routes/calendar.js
+++ server/routes/calendar.js
@@ -276,4 +276,56 @@ router.get('/:account', function(req, res, next) {
     });
 });
 
+// My Calendar Item
+router.get('/date/:date', authMiddleware, function(req, res, next) {
+    const email = req.decoded.email;
+
+    let query = "SELECT user_id, email FROM ?? WHERE ??=?";
+    const table = ["user", "email", email];
+    query = mysql.format(query, table);
+
+    db.query(query, function(err, rows) {
+        if(err) {
+            res.status(500).json({errorMsg : "Database connection error"});
+        } else {
+            if(rows.length === 1) {
+                const user_id = rows[0].user_id;
+                let query = "SELECT user_id, public FROM ?? WHERE ??=?";
+                const table = ["calendar", "user_id", user_id];
+                query = mysql.format(query, table);
+
+                db.query(query, function(err, rows) {
+                    if (err) {
+                        res.status(500).json({errorMsg: "Database connection error"});
+                    } else {
+                        let query = "SELECT item_id, todo FROM ?? WHERE ??=? AND ??=?";
+                        const table = ["calendar_item", "user_id", user_id, "date", req.params.date];
+                        query = mysql.format(query, table);
+
+                        db.query(query, function(err, rows) {
+                            if (err) {
+                                res.status(500).json({errorMsg: "Database connection error"});
+                            } else {
+                                let items = [];
+                                rows.forEach((v, k) => {
+                                    items.push({
+                                        itemId: v.item_id,
+                                        todo: v.todo,
+                                    });
+                                });
+
+                                res.json({
+                                    items: items
+                                });
+                            }
+                        });
+                    }
+                });
+            } else {
+                res.status(404).json({errorMsg : "User Not Found"});
+            }
+        }
+    });
+});
+
 module.exports = router;
