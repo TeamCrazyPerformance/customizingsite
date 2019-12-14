f2e6b8c803d5c19bde4bedce0a95da11f9614d8c
KUvH <kuvh@live.co.kr>
2019-12-14 20:08:53 +0900

    [Create Feature] Bookmark Info By Account

--- server/routes/bookmark.js
+++ server/routes/bookmark.js
@@ -274,4 +274,75 @@ router.delete('/:itemId', authMiddleware, function(req, res, next) {
     });
 });
 
+// Bookmark Info By Account
+router.get('/:account', function(req, res, next) {
+    const account = req.params.account;
+
+    let query = "SELECT user_id FROM ?? WHERE ??=?";
+    const table = ["user_account", "account", account];
+    query = mysql.format(query, table);
+
+    db.query(query, function(err, rows) {
+        if(err) {
+            res.status(500).json({errorMsg : "Database connection error"});
+        } else {
+            if(rows.length === 1) {
+                const user_id = rows[0].user_id;
+                let query = "SELECT user_id, title, description, public FROM ?? WHERE ??=?";
+                const table = ["bookmark", "user_id", user_id];
+                query = mysql.format(query, table);
+
+                db.query(query, function(err, rows) {
+                    if (err) {
+                        res.status(500).json({errorMsg: "Database connection error"});
+                    } else {
+                        let title = null;
+                        let description = null;
+                        let is_public = null;
+
+                        if(rows.length === 1) {
+                            title = rows[0].title;
+                            description = rows[0].description;
+                            is_public = !!rows[0].public;
+                        }
+
+                        if (is_public) {
+                            let query = "SELECT item_id, title, url, color, initial FROM ?? WHERE ??=?";
+                            const table = ["bookmark_item", "user_id", user_id];
+                            query = mysql.format(query, table);
+
+                            db.query(query, function(err, rows) {
+                                if (err) {
+                                    res.status(500).json({errorMsg: "Database connection error"});
+                                } else {
+                                    let items = [];
+                                    rows.forEach((v, k) => {
+                                        items.push({
+                                            itemId: v.item_id,
+                                            title: v.title,
+                                            url: v.url,
+                                            color: v.color,
+                                            initial: v.initial,
+                                        });
+                                    });
+
+                                    res.json({
+                                        title: title,
+                                        description: description,
+                                        items: items
+                                    });
+                                }
+                            });
+                        } else {
+                            res.status(403).json({errorMsg: "Not a public account"});
+                        }
+                    }
+                });
+            } else {
+                res.status(404).json({errorMsg : "Account Not Found"});
+            }
+        }
+    });
+});
+
 module.exports = router;
