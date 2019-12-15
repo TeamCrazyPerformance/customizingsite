a0b34bb19cf4828465cbfc742dcd7e1b64a435f0
KUvH <kuvh@live.co.kr>
2019-12-16 05:23:12 +0900

    [Create Feature] Album Info Editor Popup

--- client/js/album.js
+++ client/js/album.js
@@ -3,6 +3,8 @@ function getMyAlbumInfo() {
         url: "http://cs.kuvh.kr/api/album",
         headers: {"x-access-token": localStorage.token},
         success: function(data) {
+            initIEPopup(data);
+
             $(".handwritten").text(data.handwritten);
             $(".side-text").text(data.description);
 
@@ -21,6 +23,36 @@ function getMyAlbumInfo() {
             alert("로그인이 필요합니다.");
             location.href="/login.html?backUrl=/album.html";
         }
+    });
+}
+
+function initIEPopup(data) {
+    $("#handwritten").val(data.handwritten);
+    $("#description").val(data.description);
+    $("#isPublic").attr('checked', data.isPublic);
+
+    $("#ieform").submit(function(event){
+        event.preventDefault();
+        var data = {
+            handwritten: $("#handwritten").val(),
+            description: $("#description").val(),
+            isPublic: $("#isPublic").is(":checked")
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "http://cs.kuvh.kr/api/album",
+            contentType: 'application/json',
+            headers: {"x-access-token": localStorage.token},
+            data: JSON.stringify(data),
+            success: function(data) {
+                alert("정보가 수정되었습니다.");
+                location.reload();
+            },
+            error: function() {
+                alert("잠시 후 다시 시도해주세요.");
+            }
+        });
     });
 }
 
