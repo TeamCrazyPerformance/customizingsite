ae6e27fd09131b02802c16819313ca5994c9cd36
KUvH <kuvh@live.co.kr>
2019-12-16 05:55:21 +0900

    [Create Feature] Album Info By Account

--- client/js/album.js
+++ client/js/album.js
@@ -1,8 +1,24 @@
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
+
 function getMyAlbumInfo() {
     $.ajax({
         url: "http://cs.kuvh.kr/api/album",
         headers: {"x-access-token": localStorage.token},
         success: function(data) {
+            $("nav > ul").append("<li><a href=\"#iepopup\" rel=\"modal:open\" class=\"fas fa-edit\"></a></li>\n" +
+                "                <li><a href=\"#addpopup\" rel=\"modal:open\" class=\"fas fa-plus\"></a></li>");
+
             initIEPopup(data);
             initAddPopup();
 
@@ -26,6 +42,32 @@ function getMyAlbumInfo() {
         }
     });
 }
+
+function getAccountAlbumInfo(account) {
+    $.ajax({
+        url: `http://cs.kuvh.kr/api/album/${account}`,
+        success: function(data) {
+            $(".handwritten").text(data.handwritten);
+            $(".side-text").text(data.description);
+
+            $.each(data.items, function(i, obj) {
+                const itemId = obj.itemId;
+                const imgKey = obj.imgKey;
+                const title = obj.title;
+                const description = obj.description;
+                const d = new Date(obj.date);
+                const date = d.getFullYear() + '.' + (d.getMonth() + 1) + '.' + d.getDate();
+
+                $(".gg-box").append(`<div class=\"gg-element\" data-id=\"${itemId}\" style=\"background-image:url(https://customizingsite.s3.ap-northeast-2.amazonaws.com/${imgKey})"\><div><br><br><span>${date}</span><h3>${title}</h3><p>${description}</p></div></div>`);
+            });
+        },
+        error: function() {
+            alert("존재하지 않는 사용자거나 비공개 상태입니다.");
+            history.back();
+        }
+    });
+}
+
 
 function initIEPopup(data) {
     $("#ieform input[name='handwritten']").val(data.handwritten);
@@ -88,5 +130,10 @@ function initAddPopup() {
 }
 
 $(document).ready(function() {
-    getMyAlbumInfo();
+    const account = getUrlVars()['account'];
+    if(account) {
+        getAccountAlbumInfo(account);
+    } else {
+        getMyAlbumInfo();
+    }
 });
